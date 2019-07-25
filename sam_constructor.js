997ff88d662137cf3117fd780d5412559ebdb0f8
Samson Arsanios <ssamseat@hotmail.com>
2019-07-25 14:54:39 +0300

    Update sam_constructor.js

--- sam_constructor.js
+++ sam_constructor.js
@@ -1,3 +1,4 @@
+
 /* defining objects with parameters */
 
 function book(title,author,publishedDate,genre, publisher){
@@ -11,7 +12,7 @@ function book(title,author,publishedDate,genre, publisher){
 var book1 = new book("Chronicles of Narnia","C.S. Lewis",1950,"Fantasy","Penguin Classics");
 var book2 = new book("Dunk and Egg","George R.R Martin",1996,"Fiction","Stevenson");
 var book3 = new book("Chronicles of Narnia","C.S. Lewis",1950,"Fantasy","Penguin Classics");
-// loop that iterates through the arguments passed to object
+// loop that iterates through the properties passed to object
 for (let i in book1){
     console.log("Book: "+book1[i]);
 }
@@ -29,7 +30,7 @@ var weather1 = new weather("Very high","low humidity","high pressure","on the ri
 var weather2 = new weather("very low","high humidity","low pressure","on the rise","thunder");
 var weather3 = new weather("Chronicles of Narnia","C.S. Lewis",1950,"Fantasy","Penguin Classics");
 
-// loop that iterates through the arguments passed to object book
+// loop that iterates through the properties passed to object book
 for (let i in weather1){
     // print out value to the console
     console.log(weather1[i]);
@@ -47,7 +48,7 @@ function fish(name,type,gender,age,weight){
 let fish1 = new fish("Willy","Dolphin","Male",5,50)
 let fish2 = new fish("Nemo","Blue Whale","Male",3,300)
 let fish3 = new fish("Dorry","Shark","Female",10,90)
-// loop that iterates through the arguments passed to object
+// loop that iterates through the properties passed to object
 for (let i in fish1){
   console.log(fish1[i])
 }
@@ -64,7 +65,7 @@ function taxi(name,color,numberPlate,seats,engineType){
 let taxi1 = new taxi("toyota","yellow","UAJ45J",4, "VX5")
 let taxi2 = new taxi("mazda","oange","URT67I",8, "VR5")
 let taxi3 = new taxi("tata","blue","UET5H",3, "V1G")
-// loop that iterates through the arguments passed to object
+// loop that iterates through the properties passed to object
 for(let i in taxi1){
   console.log(taxi1[i])
 }
@@ -81,7 +82,7 @@ function courseUnits(year,semester,duration,type,difficulty){
 let courseUnits1 = new courseUnits(2019,4,"4 months","Accelerated","Easy")
 let courseUnits2 = new courseUnits(2018,3,"5 months","Extensive","Medium")
 let courseUnits3 = new courseUnits(2017,1,"6 months","Accelerated","Hard")
-// loop that iterates through the arguments passed to object
+// loop that iterates through the properties passed to object
 for(let i in courseUnits1){
     console.log(courseUnits1[i])
 }
